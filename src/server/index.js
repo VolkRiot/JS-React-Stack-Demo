44537853447c31e8d425ae1ae29cd24a6b05db47
Mikhail Metrikin <metrikin@gmail.com>
2017-06-19 23:33:59 -0700

    Additional Stack example

--- src/server/index.js
+++ src/server/index.js
@@ -5,6 +5,8 @@ import express from 'express'
 import { APP_NAME, STATIC_PATH, WEB_PORT } from '../shared/config'
 import { isProd } from '../shared/util'
 import renderApp from './render-app'
+import { helloEndpointRoute } from '../shared/routes'
+
 
 const app = express()
 
@@ -14,6 +16,10 @@ app.use(STATIC_PATH, express.static('public'))
 
 app.get('/', (req, res) => {
   res.send(renderApp(APP_NAME))
+})
+
+app.get(helloEndpointRoute(), (req, res) => {
+  res.json({ serverMessage: `Hello from the server! (recieved ${req.params.num})` })
 })
 
 app.listen(WEB_PORT, () => {
