7ac0d5500cda67bf6e477d7b941ecec1bbfd63a0
Mikhail Metrikin <metrikin@gmail.com>
2017-07-30 17:40:08 -0700

    Adding Helmet and refactored

--- src/server/render-app.jsx
+++ src/server/render-app.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOMServer from 'react-dom/server'
 import { Provider } from 'react-redux'
 import { StaticRouter } from 'react-router'
+import Helmet from 'react-helmet'
 
 import initStore from './init-store'
 import App from './../shared/app'
@@ -17,12 +18,14 @@ const renderApp = (location, plainPartialState, routerContext = {}) => {
       </StaticRouter>
     </Provider>,
   )
+  const head = Helmet.rewind()
   return `<!doctype html>
-   <html>
-     <head>
-       <title>FIX ME</title>
-       <link rel="stylesheet" href="${STATIC_PATH}/css/style.css">
-     </head>
+    <html>
+      <head>
+        ${head.title}
+        ${head.meta}
+        <link rel="stylesheet" href="${STATIC_PATH}/css/style.css">
+      </head>
      <body>
        <div class="${APP_CONTAINER_CLASS}">${appHtml}</div>
        <script>
